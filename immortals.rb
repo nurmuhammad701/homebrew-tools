6281679c098ec4df5a5daae373491b12dd52f89e
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-09-08 17:27:40 +0500

    All

--- immortals.rb
+++ immortals.rb
@@ -2,7 +2,7 @@ class Immortals < Formula
     desc "Collection of CLI tools including todo and currency converter"
     homepage "https://github.com/nurmuhammad701/immortals_brew"
     url "https://github.com/nurmuhammad701/immortals_brew/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "3aa2f78413ab1ef437a9899d5042c94d5cde351fa6264b88074d7cbb90ad4eca"
+    sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
     license "MIT"
   
     depends_on "go" => :build
