92fde1ad0aa0ed4989f22dcfd4dfef933058731a
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-09-08 17:39:55 +0500

    Update formula to explicitly set working directory and environment variables

--- immortals.rb
+++ immortals.rb
@@ -1,19 +1,27 @@
 class Immortals < Formula
-    desc "Collection of CLI tools including todo and currency converter"
-    homepage "https://github.com/nurmuhammad701/immortals_brew"
-    url "https://github.com/nurmuhammad701/immortals_brew/archive/refs/tags/v1.0.0.tar.gz"
-    sha256 "3aa2f78413ab1ef437a9899d5042c94d5cde351fa6264b88074d7cbb90ad4eca"
-    license "MIT"
-  
-    depends_on "go" => :build
-  
-    def install
+  desc "Collection of CLI tools including todo and currency converter"
+  homepage "https://github.com/nurmuhammad701/immortals_brew"
+  url "https://github.com/nurmuhammad701/immortals_brew/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "3aa2f78413ab1ef437a9899d5042c94d5cde351fa6264b88074d7cbb90ad4eca"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["GOPATH"] = buildpath
+    ENV["GO111MODULE"] = "on"
+    
+    system "ls", "-la"  # This will list the contents of the current directory
+    system "cat", "go.mod"  # This will show the contents of go.mod if it exists
+    
+    cd buildpath do
       system "go", "build", "-o", bin/"todo", "./todos"
       system "go", "build", "-o", bin/"currency", "./Currency"
     end
-  
-    test do
-      system "#{bin}/todo", "--help"
-      system "#{bin}/currency", "--help"
-    end
   end
+
+  test do
+    system "#{bin}/todo", "--help"
+    system "#{bin}/currency", "--help"
+  end
+end
