5b2ffab6e150a06070237b6c61f7f014ae1bd195
nurmuhammad701 <nurmuhammadmel@gmail.com>
2024-09-09 09:07:41 +0500

    all

--- immortals.rb
+++ immortals.rb
@@ -1,8 +1,8 @@
 class Immortals < Formula
-  desc "Collection of CLI tools including todo and currency converter"
+  desc "Collection of CLI tools including todo, currency converter, calculator, and file manager"
   homepage "https://github.com/nurmuhammad701/immortals_brew"
-  url "https://github.com/nurmuhammad701/immortals_brew/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "cbe64c324f9f70aa84a7438fe0d0de63f1d26bf793ce1b9f2ccd712a3e8dfa77"
+  url "https://github.com/nurmuhammad701/immortals_brew/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "12183f812cdd41c170877787c0c72ef564669a77d6f3e046b36a9c67ed17540a"
   license "MIT"
 
   depends_on "go" => :build
@@ -11,17 +11,16 @@ class Immortals < Formula
     ENV["GOPATH"] = buildpath
     ENV["GO111MODULE"] = "on"
     
-    system "ls", "-la"  # This will list the contents of the current directory
-    system "cat", "go.mod"  # This will show the contents of go.mod if it exists
-    
-    cd buildpath do
-      system "go", "build", "-o", bin/"todo", "./todos"
-      system "go", "build", "-o", bin/"currency", "./Currency"
-    end
+    system "go", "build", "-o", bin/"todo", "./todos"
+    system "go", "build", "-o", bin/"currency", "./Currency"
+    system "go", "build", "-o", bin/"calculator", "./calculator"
+    system "go", "build", "-o", bin/"filemanager", "./files"
   end
 
   test do
     system "#{bin}/todo", "--help"
     system "#{bin}/currency", "--help"
+    system "#{bin}/calculator", "--help"
+    system "#{bin}/filemanager", "--help"
   end
 end
